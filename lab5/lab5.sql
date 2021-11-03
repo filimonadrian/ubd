15aa23aefa34983d1e5be69b6611e066eba79f8c
filimonadrian <adrian.filimon@hotmail.com>
2021-11-03 11:43:31 +0200

    Finished lab 5

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -10,7 +10,7 @@ insert into login_lab_ubd values( 'Filimon Adrian', '342C2', 'Lab5', user, sysda
 
 -- 1 informatii despre fisierele de log si starea lor
 desc v$logfile
-select * form v$logfile
+select * from v$logfile;
 
 -- 2 Informatii legate de modul de lucru al bazei de date ( cu arhivare sau fara arhivare a fisierelor de
 -- log, data cand au fost resetate fisierele de log, etc.)
@@ -30,23 +30,23 @@ select group#, members, bytes, archived, status from v$log;
 
 -- !!!!
 -- 5) adaugarea unui membru la un grup (se creeaza automat in SO un nou fisier de log)
-alter database add logfile member <path> to group 1;
+alter database add logfile member 'D:\log2.rdo' to group 1;
 
 -- 6) resetarea unui grup dintr-un fisier de log(care nu este grup curent)
 alter database clear logfile group 2;
 
 -- 7) relocarea unui fisier de log cu baza de date online(trebuie ca fisierul 
--- sa nu fie ingrupul curent sau sa exista o copie in noua locatie)
-alter database rename file <file_path> to <file_path>;
+-- sa nu fie in grupul curent sau sa exista o copie in noua locatie)
+alter database rename file 'D:\log2.rdo' to 'D:\new_log.rdo';
 
 -- 8 stergerea unui membru dintr-un grup:
 --  - se verifica starea fisierului care va fi sters
-select * from v$logfile
+select * from v$logfile;
 --  - se sterge fisierul de log(trebuie sa nu fie in grupul curent)
-alter database drop logfile member <file_path>;
+alter database drop logfile member 'D:\new_log.rdo';
 -- daca fisierul face parte din grupul curent, se face switch pe urmatorul grup
     alter system switch logfile;
-    alter database drop logfile member <file_path>;
+    alter database drop logfile member 'D:\new_log.rdo';
     select * from v$logfile;
 
 -- 9) informatii legate de modul de lucru al instantei
@@ -73,3 +73,57 @@ select * from v$controlfile_record_section where type='TEMPORARY FILENAME';
 -- 3) Faceti o lista cu grupurile fisierelor de log, fisierele membru, calea unde
 -- sunt create fizic si dimensiunile lor in MB
 select GROUP#, MEMBER from v$logfile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 4) aratati din dictionar numarul de inregistrari care pot fi stocate si numarul
+-- de inregistrari utilizate pentru parametrii aferenti fisierelor de date si
+-- tablespace-urile create pe baza de date curenta
+select * from v$controlfile_record_section where type='TABLESPACE' OR type = 'DATAFILE';
+
+-- 5) Faceti o lista cu grupurile fisierelor de log, fisierele membru, calea unde
+-- sunt create fizic si dimensiunile lor in MB
+select
+    a.group#,
+    a.member,
+    b.bytes,
+    b.members
+FROM
+    v$logfile a,
+    v$log b
+wHERE
+    a.group# = b.group#;
+
+select * from v$logfile;
+select bytes from v$log;
+
+
+
+-- 6) aratati din dictionar numele instantei curente si grupurile fisierelor log
+-- aflate in starea 'OPEN'
+
+select
+    a.instance_name,
+    b.groups,
+    b.current_group#,
+    b.sequence#,
+    b.status
+FROM
+    v$instance a,
+    v$thread b
+wHERE
+    a.instance_name = b.instance;
+
+select instance, groups, current_group#, sequence#, instance, status from v$thread;
