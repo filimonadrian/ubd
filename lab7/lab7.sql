36e6a950afee1b4ae6db13a4c84618c448142094
filimonadrian <adrian.filimon@hotmail.com>
2021-11-17 14:41:25 +0200

    finished lab7

--- lab7/lab7.sql
+++ lab7/lab7.sql
@@ -12,19 +12,18 @@ select tablespace_name, file_id, file_name, blocks, bytes from dba_data_files wh
 
 -- 3) Informatii despre segmentele de tip tabela create intr-un tablespace :
 desc dba_segments;
-select owner,segment_name,segment_type, tablespace_name, blocks, extents from dba_segments where owner='SCOTT'and segment_type='TABLE';
+select owner,segment_name,segment_type, tablespace_name, blocks, extents from dba_segments where owner='ABD1'and segment_type='TABLE';
 
 
 -- 4) Informatii despre dimensiunea extensiilor alocate unui segment:
 desc dba_extents
-select owner, segment_name, segment_type, tablespace_name from dba_extents where owner='SCOTT' and segment_name='EMP';
-select segment_name, extent_id, file_id, block_id, blocks, bytes from dba_extents where owner='SCOTT' and segment_name='EMP';
+select owner, segment_name, segment_type, tablespace_name from dba_extents where owner='ABD1' and segment_name='EMP';
+select segment_name, extent_id, file_id, block_id, blocks, bytes from dba_extents where owner='ABD1' and segment_name='EMP';
 
 -- 5) Informatii despre tablespace, fisierul de date alocat, numarul de blocuri si spatiul liber in fiecare bloc :
 desc dba_free_space
 select * from dba_free_space where tablespace_name='USERS';
 select tablespace_name, count(*), max(blocks), sum(blocks) from dba_free_space group by tablespace_name;
-
 select tablespace_name, blocks from dba_free_space where tablespace_name='SYSTEM';
 
 -- 6) Unificarea spatiilor contigue dintr-un tablespace(defragmentare):
@@ -63,16 +62,21 @@ select username, sid, saddr, service_name from v$session;
 
 -- 13) Informatii despre tranzactii( adresele tranzactiilor pot fi relationate cu sesiunile prin ses_addr):
 desc v$transaction
-insert into emp values (999, 'TEST','TRANZACT',1111,sysdate, 100,0,10)
-select addr, xidusn, used_ublk,start_uext, start_ubafil from v$transaction
+insert into emp values (999, 'TEST','TRANZACT',1111,sysdate, 100,0,10);
+select addr, xidusn, used_ublk,start_uext, start_ubafil from v$transaction;
 
 
 -- 14) Informatii despre blocurile de undo folosite de tranzactia curenta:
-SELECT s.username, t.xidusn, t.ubafil, t.ubablk, t.used_ublk FROM v$session s, v$transaction t WHERE s.saddr = t.ses_addr;
+SELECT s.username, t.xidusn, t.ubafil, t.ubablk, t.used_ublk
+FROM
+    v$session s,
+    v$transaction t
+WHERE
+    s.saddr = t.ses_addr;
 
 -- 15) Statistici privind dimensiunea spatiului de undo:
 desc v$undostat
-SELECT to_char(begin_time, ’dd-mm-yyyy hh:mi:ss’) start_time, to_char(end_time,’ddmm-yyyy hh:mi:ss’) end_time, ((end_time-begin_time)* 24)*60 minute, undoblks FROM v$undostat;
+SELECT to_char(begin_time, 'dd-mm-yyyy hh:mi:ss') start_time, to_char(end_time, 'ddmm-yyyy hh:mi:ss') end_time, ((end_time-begin_time)* 24)*60 minute, undoblks FROM v$undostat;
 SELECT (SUM(undoblks) / SUM ((end_time - begin_time) * 24*60*60)) nr_med_blocuri_undo_sec FROM v$undostat;
 
 
@@ -88,13 +92,63 @@ select tablespace_name,max_sort_size,extent_size,max_sort_blocks from v$sort_seg
 
 
 -- 19) Informatii despre sesiuni si tablespace-ul in care se afla segmentele temporare de sortare folosite in sesiunea curenta:
-desc v$tempseg_usage ( v$sort_usage)
+desc v$tempseg_usage 
+desc v$sort_usage
 
 
 -- 20) Setarea zonei de memorie utilizata pentru sortare in sesiunea curenta la 10K.
 alter system set sort_area_size=10240 deferred
 
+
+
+
+
+
+
+
+
+
 -- exercitii individuale
 
 
+-- 1) Sa se arate din dictionar care sunt indecsii creati pe tabelele din userul ABD1, in ce tabele sunt creati si cate blocuri le sunt alocate
+select owner, blocks, segment_name, segment_type, tablespace_name from dba_segments where owner='ABD1' and segment_type='INDEX';
 
+
+-- 2) Sa se arate din dictionar care este spatiul liber, ca numar de blocuri, in tablespace-ul permanent asignat userului curent
+select
+    tablespace_name,
+    sum(blocks) free_blocks
+from
+    dba_free_space
+where
+    tablespace_name = (select default_tablespace from dba_users where username = user) group by tablespace_name;
+
+
+-- 3) Faceti o lista cu numele segmentului asociat cheii primare a tabelei EMP, numele fisierului in care este stocat si dimensiunea extensiei in numar de blocuri
+select a.segment_name, a.blocks, b.file_name
+from dba_extents a, dba_data_files b
+where
+    a.file_id = b.file_id AND a.segment_name = 'EMP';
+
+
+
+
+-- exercitii din laboratoarele altor grupe
+
+-- 1) Sa se arate din dictionar care sunt tabelele partitionate create de userul SYSTEM
+select tablespace_name, partition_name from dba_segments where segment_type LIKE 'TABLE PARTITION' and owner='SYSTEM';
+
+-- 2) Aratati din dictionar dimensiunea in blocuri si bytes a indexului creat pentru cheia primara a tabelei emp
+select bytes, blocks, segment_name
+from user_segments
+where segment_name in (SELECT constraint_name from user_constraints where table_name = 'EMP' and constraint_type = 'P');
+
+-- 3) Aratati din dictionar numele si numarul total de blocuri libere din tablespace-ul permanent asignat userului curent
+select
+    tablespace_name,
+    sum(blocks) free_blocks
+from
+    dba_free_space
+where
+    tablespace_name = (select default_tablespace from dba_users where username = user) group by tablespace_name;
